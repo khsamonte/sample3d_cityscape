7b174b5e33e948ab0c22d006d4274ad69b3067d5
Ken Samonte <khsamonte@gmail.com>
2025-02-26 15:20:13 +0800

    Re-trigger deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/sample3d_cityscape/",
+  build: {
+    outDir: "dist", // Make sure the output directory is set to dist
+  },
 });
